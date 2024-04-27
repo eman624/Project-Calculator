43868fae4f4bcfe4d9a90e0ab0f95b220a987c7b
Edgar <edgar.noel.m@gmail.com>
2024-04-26 18:01:10 -0700

    added base files

---
+++ operators.js
@@ -0,0 +1,32 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function operator(operator, x, y) {
+  switch (operator) {
+    case add:
+      add(x, y);
+      break;
+    case subtract:
+      subtract(x, y);
+      break;
+    case multiply:
+      multiply(x, y);
+      break;
+    case divide:
+      divide(x, y);
+      break;
+  }
+}
